7bb23d92136926f7acb7a5ee810501032db36bf6
Dezso <bauer.dezso@gmail.com>
2019-01-17 14:55:06 +0100

    Used key combination for auto-ident the file

--- week01/day04/src/swapNumbers.java
+++ week01/day04/src/swapNumbers.java
@@ -6,9 +6,9 @@ public class swapNumbers {
 
         System.out.println("a = " + (a));
         System.out.println("b = " + (b));
-        System.out.println( "The a was: " + a + " and now  swapd with b " + ((a +b) - a));
-        System.out.println( "The b was: " + b + " and now  swapd with a " + ((a +b) - b));
-        
+        System.out.println("The a was: " + a + " and now  swapd with b " + ((a + b) - a));
+        System.out.println("The b was: " + b + " and now  swapd with a " + ((a + b) - b));
+
     }
 
 
