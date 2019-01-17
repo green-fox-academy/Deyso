adb2c0d6342a386040b2b76ea7ad7e596764e3e9
Dezso <bauer.dezso@gmail.com>
2019-01-17 13:08:41 +0100

    Introduce yourself made with variables

---
+++ week01/day04/src/introduceYourself.java
@@ -0,0 +1,13 @@
+public class introduceYourself {
+    public static void main(String[] args) {
+
+        String name = "Dezso Bauer";
+        int age = 23;
+        double height = 1.78;
+        String unit = "m"; //could be feet, cm or something else
+
+        System.out.println("Your name is " + name);
+        System.out.println("Your age is " + age);
+        System.out.println("Your height is " + height + unit);
+    }
+}
