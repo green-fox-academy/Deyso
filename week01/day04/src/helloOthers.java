d3fbc28bfa20aa44c8ae8b5c9459df46e5427dc2
Dezso <bauer.dezso@gmail.com>
2019-01-17 13:00:30 +0100

    Hello others greeting

---
+++ week01/day04/src/helloOthers.java
@@ -0,0 +1,13 @@
+public class helloOthers {
+    public static void main(String[] args) {
+
+        String firstPerson = "Gabi";
+        String secoundPerson = "Laci";
+        String  thirdPerson = "Zsofi";
+        String greetings = "Hello";
+
+        System.out.println(greetings + " " + firstPerson);
+        System.out.println(greetings + " " + secoundPerson);
+        System.out.println(greetings + " " + thirdPerson);
+    }
+}
