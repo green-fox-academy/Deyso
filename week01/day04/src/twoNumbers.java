b8d9ac4f5d22d8ba26c4758fa5450be29f0e7d11
Dezso <bauer.dezso@gmail.com>
2019-01-17 14:00:26 +0100

    two numbers

---
+++ week01/day04/src/twoNumbers.java
@@ -0,0 +1,20 @@
+public class twoNumbers {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 13;
+        int secoundNumber = 22;
+        double firstNumberDec = firstNumber;
+        double secoundNumberDec = secoundNumber;
+
+        System.out.println("first | " + (firstNumber + secoundNumber));
+        System.out.println("substracted |  " + (firstNumber - secoundNumber));
+        System.out.println("multiplied |  " + (firstNumber * secoundNumber));
+        System.out.println("divided |  " + (secoundNumberDec / firstNumberDec));
+        System.out.println("integer |  " + (secoundNumber / firstNumber));
+        System.out.println("remainder |  " + (secoundNumber % firstNumber));
+
+
+    }
+
+}
