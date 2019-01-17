e8f520d1910ff96191e54e8bf2cae923fd5956a3
Dezso <bauer.dezso@gmail.com>
2019-01-17 14:53:06 +0100

    swaped the numbers with variables

---
+++ week01/day04/src/swapNumbers.java
@@ -0,0 +1,15 @@
+public class swapNumbers {
+    public static void main(String[] args) {
+
+        int a = 123;
+        int b = 526;
+
+        System.out.println("a = " + (a));
+        System.out.println("b = " + (b));
+        System.out.println( "The a was: " + a + " and now  swapd with b " + ((a +b) - a));
+        System.out.println( "The b was: " + b + " and now  swapd with a " + ((a +b) - b));
+        
+    }
+
+
+}
