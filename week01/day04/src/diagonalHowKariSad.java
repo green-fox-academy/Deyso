9d7501275a26bfbc8b5fa565de9ddc58c7da4c61
Dezso <bauer.dezso@gmail.com>
2019-01-18 16:00:23 +0100

    diagonals

---
+++ week01/day04/src/diagonalHowKariSad.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class diagonalHowKariSad {
+
+    public static void main(String[] args) {
+
+        int squareNumber;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give a number");
+        squareNumber = input.nextInt();
+
+
+        for (int i = 0; i < squareNumber; ++i) {
+            for (int j = 0; j < squareNumber; j++) {
+                if (i == 0 || i == squareNumber-1) {
+                    System.out.print("%");
+                } else if (j == 0 || j == squareNumber-1) {
+                    System.out.print("%");
+                } else if (i != 0 || i != squareNumber-1) {
+                    System.out.print(" ");
+                } else if (j != 0 || j != squareNumber-1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+
+    }
+}
--- week01/day04/src/inverseDiagonal.java
+++ week01/day04/src/inverseDiagonal.java
@@ -1,7 +1,7 @@
 import java.util.Collections;
 import java.util.Scanner;
 
-public class drawDiagonal {
+public class inverseDiagonal {
     public static void main(String[] args) {
 
         // %%%%%%
@@ -11,21 +11,28 @@ public class drawDiagonal {
         // %   %%
         // %%%%%%
 
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(i  +""+ j + " ");
+            }
+            System.out.println();
+        }
+
         int squareHigh;
 
         Scanner input = new Scanner(System.in);
         System.out.println("How big you want the square (in lines)");
         squareHigh = input.nextInt();
 
-        int space = squareHigh - 3;
-        int spaceForDiagonal = 0;
+        int space = 0;
+        int spaceForDiagonal = squareHigh - 3;
+
 
         for (int i = 0; i < squareHigh; i++) {
 
             if (i == squareHigh - 1) {
                 for (int j = 0; j < squareHigh; j++) {
                     System.out.print("%");
-
                 }
                 System.out.println();
             } else if (i == 0) {
@@ -34,14 +41,15 @@ public class drawDiagonal {
                 }
                 System.out.println();
             } else if (i != squareHigh && i != 0) {
+
                 System.out.print("%");
                 System.out.print(String.join("", Collections.nCopies(spaceForDiagonal, " ")));
                 System.out.print("%");
                 System.out.print(String.join("", Collections.nCopies(space, " ")));
                 System.out.print("%");
                 System.out.println();
-                spaceForDiagonal++;
-                space--;
+                spaceForDiagonal--;
+                space++;
             }
         }
     }
