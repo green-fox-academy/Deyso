608c9c8b380630865642afcda74a8e4c9e6fd99c
Dezso <bauer.dezso@gmail.com>
2019-01-18 13:15:02 +0100

    made drawSquare with scannar

---
+++ week01/day04/src/drawSquare.java
@@ -0,0 +1,44 @@
+import java.util.Collections;
+import java.util.Scanner;
+
+public class drawSquare {
+    public static void main(String[] args) {
+
+        // %%%%%%
+        // %    %
+        // %    %
+        // %    %
+        // %    %
+        // %%%%%%
+
+        int squareHigh;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("How big you want the square (in lines)");
+        squareHigh = input.nextInt();
+
+        int space = squareHigh - 2;
+
+        for (int i = 0; i < squareHigh; i++) {
+
+            if (i == squareHigh - 1) {
+                for (int j = 0; j < squareHigh; j++) {
+                    System.out.print("%");
+
+                }
+                System.out.println();
+            } else if (i == 0) {
+                for (int k = 0; k < squareHigh; k++) {
+                    System.out.print("%");
+                }
+                System.out.println();
+            } else if (i != squareHigh && i != 0) {
+                System.out.print("%");
+                System.out.print(String.join("", Collections.nCopies(space, " ")));
+                System.out.print("%");
+                System.out.println();
+            }
+
+        }
+    }
+}
