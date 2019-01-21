297bdd76e4b043c33663592946591367e4eb2b52
Dezso <bauer.dezso@gmail.com>
2019-01-21 14:55:55 +0100

    third made with arrays

---
+++ week02/day01/src/third.java
@@ -0,0 +1,11 @@
+public class third {
+    public static void main(String[] args) {
+        // - Create an array variable named `q`
+        //   with the following content: `[4, 5, 6, 7]`
+        // - Print the third element of `q`
+
+        int [] q = new int[]{4,5,6,7};
+        System.out.println(q[2]);
+    }
+
+}
