c74c918809ae27038e3248667d12d585e970dd0c
Dezso <bauer.dezso@gmail.com>
2019-01-21 14:50:31 +0100

    factorio name changing

---
+++ week02/day01/src/factorio.java
@@ -0,0 +1,21 @@
+public class factorio {
+    public static void main(String[] args) {
+
+        //  Create the usual class wrapper
+        //  and main method on your own.
+
+        // - Create a function called `factorio`
+        //   that returns it's input's factorial
+
+        String text = "factorio";
+        factorio(text);
+        System.out.println(factorio(text));
+    }
+
+    public static String factorio(String atadott){
+
+        atadott = "factorial";
+        return atadott;
+    }
+
+}
