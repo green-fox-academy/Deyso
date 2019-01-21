235d7a2c8e01c174fe2ec67f903bc7be42ac34e5
Dezso <bauer.dezso@gmail.com>
2019-01-21 23:47:23 +0100

    reverseList

---
+++ week02/day01/src/reserveList.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class reserveList {
+    public static void main(String[] args) {
+        // - Create an array variable named `aj`
+        //   with the following content: `[3, 4, 5, 6, 7]`
+        // - Reverse the order of the elements in `aj`
+        // - Print the elements of the reversed `aj`
+
+        int[] aj = {3, 4, 5, 6, 7};
+        int temp;
+        for (int i = 0; i < aj.length / 2; i++) {
+            temp = aj[i];
+            aj[i] = aj[aj.length - 1 - i];
+            aj[aj.length - 1 - i] = temp;
+        }
+        System.out.println(Arrays.toString(aj));
+    }
+}
