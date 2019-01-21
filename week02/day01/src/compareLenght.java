cdda0c9fb83efd8a5055e5da9a6c35206d34c241
Dezso <bauer.dezso@gmail.com>
2019-01-21 15:00:53 +0100

    compareLenght checked the arrays with .lenght

---
+++ week02/day01/src/compareLenght.java
@@ -0,0 +1,17 @@
+public class compareLenght {
+
+    public static void main(String[] args) {
+        // - Create an array variable named `p1`
+        //   with the following content: `[1, 2, 3]`
+        // - Create an array variable named `p2`
+        //   with the following content: `[4, 5]`
+        // - Print if `p2` has more elements than `p1`
+
+        int [] p1 = new int[]{1,2,3};
+        int [] p2 = new int[]{4,5};
+
+        if (p1.length>p2.length){
+            System.out.println("p1 has more elements than p2");
+        }
+    }
+}
