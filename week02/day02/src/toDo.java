87f8f6c2afbf9c592038d8d41c197e043692cae7
Dezso <bauer.dezso@gmail.com>
2019-01-22 16:11:16 +0100

    toDo made with .startsWith .endsWith

--- week02/day02/src/toDo.java
+++ week02/day02/src/toDo.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class toDo {
     public static void main(String[] args) {
 
+
          /* // Add "My todo:" to the beginning of the todoText
                 // Add " - Download games" to the end of the todoText
                 // Add " - Diablo" to the end of the todoText but with indention
@@ -16,19 +17,25 @@ public class toDo {
 */
 
 
+        String todoText = " - Buy milk\n";
 
         String newLine = "\n - ";
         String newLineNewIndention = "\n\t - ";
-        String todoText ="My todo:" + newLine + "Buy milk" + newLine + "Download games"+ newLineNewIndention + "Diablo";
 
+        if(todoText.startsWith(" -")){
+           todoText = todoText.replace(" - Buy milk\n","My todo: ");
+        }
+        if (todoText.endsWith(": ")){
+            todoText += newLine + "Buy milk" ;
+        }
+        if (todoText.endsWith("milk")){
+            todoText += newLine + "Download Games";
+        }
+        if(todoText.endsWith("es")){
+            todoText += newLineNewIndention + "Diablo";
+        }
 
         System.out.println(todoText);
-
-
-
-
-
-
 
     }
 
