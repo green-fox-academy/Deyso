54035c46b0e7ea71a412bb0bd4ce2aced840d051
Dezso <bauer.dezso@gmail.com>
2019-01-30 12:43:41 +0100

    Animal eat(),drink() object

---
+++ week03/day03/animal/src/animals/Main.java
@@ -0,0 +1,23 @@
+package animals;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal dog = new Animal("dog");
+        dog.eat();
+        dog.play();
+        dog.eat();
+        dog.drink();
+        dog.getStatus();
+        Animal cat = new Animal("cat");
+        cat.play();
+        cat.eat();
+        cat.getStatus();
+
+
+
+
+
+
+    }
+}
