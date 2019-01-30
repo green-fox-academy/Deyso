7f7c11f1b0d9766b105bb3281c5947d7fa9099c2
Dezso <bauer.dezso@gmail.com>
2019-01-30 14:19:13 +0100

    counter missing the copy paste

---
+++ week03/day03/counter/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Counter counting = new Counter(6);
+        counting.get();
+        counting.add(6);
+        counting.get();
+
+
+    }
+
+
+}
