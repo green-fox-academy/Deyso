665bb61de0ce137fe6ad1cf909c6449b1744a009
Dezso <bauer.dezso@gmail.com>
2019-02-06 12:07:22 +0100

    Clone made with clone()

---
+++ week04/day03/clonable/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student john = new Student("John", 20, "male", "BME");
+        Student cloneOfJohn = john.clone();
+        cloneOfJohn.introduce();
+
+
+
+    }
+}
---
+++ week04/day03/clonable/src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    String name = "Jane Doe";
+    int age = 30;
+    String gender = "female";
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Person(){
+
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + " a " + age + " year old " + gender);
+    }
+
+    public void getGoal() {
+        System.out.println("My goal is: Live for the moment!");
+    }
+
+}
